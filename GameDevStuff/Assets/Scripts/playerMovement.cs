87592a316453bb23a0f89b369ee3012e2806fabe
XavierFox12 <xfactor_fox12@yahoo.com>
2017-04-20 11:12:10 -0400

    Fixed the Title Screen to start the game
    The user can now hit the start button and start the game

--- GameDevStuff/Assets/Scripts/playerMovement.cs
+++ GameDevStuff/Assets/Scripts/playerMovement.cs
@@ -6,25 +6,26 @@ using UnityEngine.SceneManagement;
 public class playerMovement : MonoBehaviour
 {
 
-    public int playerNumber = 1;
-    public GameObject shot;
-    public Transform shotSpawn;
-    public Slider healthSlider;
-    public Text winText;
-    public cameraMovement cameraScript;
-    public GameManager gameManagerScript;
-    public float speed = 10f;
-    public int health = 10;
-    private string fire;
-    private string movementVertical;
-    private string movementHorizontal;
-    private Rigidbody2D rb;
-    private SpriteRenderer spriteR;
-    private float movementInputHorizontal;
-    private float movementInputVertical;
-    private float nextFire;
-    private float fireRate;
-    private int ammo = 1000;
+    public int playerNumber = 1;                // Determines what player this script belongs to
+    public GameObject shot;                     // The shot gameobject that allows the player to shoot
+    public Transform shotSpawn;                 // The shot spawn, which is where the shot will spawn from
+    public Slider healthSlider;                 // Controller for the players health
+    public Text winText;                        // The text that displays "You Win" if the player reaches the end
+    public cameraMovement cameraScript;         // The camera script that will keep its eye on the player
+    public GameManager gameManagerScript;       // The game manager, which will determine if the game is over for the player
+    public Car carScript;                       // The car script that will be called if the player picks up scrap or gas
+    public float speed = 10f;                   // Controls the speed of the player
+    public int health = 10;                     // Controls the health of the player
+    private string fire;                        // Determines if the player shooting is 1 or 2
+    private string movementVertical;            // Allows the player to move vertically
+    private string movementHorizontal;          // Allows the player to move horizontally
+    private Rigidbody2D rb;                     // Grabs the 2d rigidbody off the player
+    private SpriteRenderer spriteR;             // Grabs the sprite renderer off the player
+    private float movementInputHorizontal;      // Controls the input for moving horizontally
+    private float movementInputVertical;        // Controls the input for moving vertically
+    private float nextFire;                     // Determines how long the player has to wait before they can shoot again
+    private float fireRate;                     // Determines the fire rate of the players
+    private int ammo = 1000;                    // Controls the amount of ammo the player has
 
 
     void Awake()
@@ -86,6 +87,11 @@ public class playerMovement : MonoBehaviour
         }
         else if (other.gameObject.CompareTag("Part"))
         {
+            other.gameObject.SetActive(false);
+        }
+        else if(other.gameObject.CompareTag("Gas"))
+        {
+            carScript.gas += 10f;
             other.gameObject.SetActive(false);
         }
         else if (other.gameObject.CompareTag("Enemy"))
